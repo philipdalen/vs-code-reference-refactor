734a30828f07dbc8806be9014fd8773eb3ef0202
Philip Dalen <philip.harmse@gmail.com>
2025-01-18 20:01:41 +0100

    update packages

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,14 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    setupFiles: ['./src/utils/__tests__/setup.ts'],
+    deps: {
+      inline: ['vscode']
+    },
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 });
